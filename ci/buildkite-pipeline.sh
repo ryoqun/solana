a52848e8f49347f1a29985c1e51388d7f08c2bfa
Ryo Onodera <ryoqun@gmail.com>
2020-09-18 15:34:23 +0900

    Change queue

--- ci/buildkite-pipeline.sh
+++ ci/buildkite-pipeline.sh
@@ -177,7 +177,7 @@ all_test_steps() {
     timeout_in_minutes: 40
     artifact_paths: "log-*.txt"
     agents:
-      - "queue=cuda"
+      - "queue=aws-deploy"
 EOF
   else
     annotate --style info \
