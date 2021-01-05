94526aa24eab22464c36b47c45be140acad4c028
Ryo Onodera <ryoqun@gmail.com>
2021-01-05 23:36:43 +0900

    No jemalloc

--- measure/src/lib.rs
+++ measure/src/lib.rs
@@ -1,9 +1,2 @@
 pub mod measure;
 pub mod thread_mem_usage;
-
-#[cfg(unix)]
-extern crate jemallocator;
-
-#[cfg(unix)]
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
