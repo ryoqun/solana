acc24c1f6badb2395a182b4510100bb2b6430d81
Ryo Onodera <ryoqun@gmail.com>
2022-12-05 15:24:36 +0900

    save

--- core/src/banking_tracer.rs
+++ core/src/banking_tracer.rs
@@ -38,7 +38,7 @@ impl TracedBankingPacketSender {
         }
     }
 
-    pub fn send(&self, a: BakingPacketBatch) -> u8 {
+    pub fn send(&self, a: BankingPacketBatch) -> u8 {
         panic!();
     }
 }
