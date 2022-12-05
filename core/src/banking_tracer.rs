65d21454facf95bf1a707a0ba638750db34b0276
Ryo Onodera <ryoqun@gmail.com>
2022-12-05 15:12:21 +0900

    save

--- core/src/banking_tracer.rs
+++ core/src/banking_tracer.rs
@@ -10,6 +10,7 @@ use solana_perf::packet::PacketBatch;
 
 pub type BankingPacketBatch = (Vec<PacketBatch>, Option<SigverifyTracerPacketStats>);
 pub type BankingPacketSender = TracedSender<CrossbeamSender<BankingPacketBatch>>;
+pub type RealBankingPacketSender = CrossbeamSender<BankingPacketBatch>;
 pub type BankingPacketReceiver = CrossbeamReceiver<BankingPacketBatch>;
 
 #[derive(Default)]
@@ -24,13 +25,13 @@ impl BankingTracer {
     }
 }
 
-pub struct TracedSender<T> {
-    sender_to_banking: T,
-    mirrored_sender_to_trace: Option<T>,
+pub struct TracedSender {
+    sender_to_banking: RealBankingPacketSender,
+    mirrored_sender_to_trace: Option<RealBankingPacketSender>,
 }
 
-impl<T> TracedSender<T> {
-    fn new(sender_to_banking: T) -> Self {
+impl TracedSender<RealBankingPacketSender> {
+    fn new(sender_to_banking: RealBankingPacketSender) -> Self {
         Self {
             sender_to_banking,
             mirrored_sender_to_trace: None,
