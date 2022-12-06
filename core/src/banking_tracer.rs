a483a9895508641d570dd4fd2647dd5f626c83e0
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 22:36:29 +0900

    save

--- core/src/banking_tracer.rs
+++ core/src/banking_tracer.rs
@@ -67,9 +67,9 @@ impl BankingTraceRunner {
             }
             for (name, batch) in packet_batches_by_time.values() {
                 match name {
-                    "non-vote" => self.non_vote_channel.0.send(batch).unwrap(),
-                    "gossip" => self.gossip_vote_channel.0.send(batch).unwrap(),
-                    "tpu" => self.tpu_vote_channel.0.send(batch).unwrap(),
+                    "non-vote" => self.non_vote_channel.0.send(batch.clone()).unwrap(),
+                    "gossip" => self.gossip_vote_channel.0.send(batch.clone()).unwrap(),
+                    "tpu" => self.tpu_vote_channel.0.send(batch.clone()).unwrap(),
                 }
             }
         }
