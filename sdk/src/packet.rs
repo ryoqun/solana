dde0a243ca9845522aacf94b1e472a3023e79739
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 16:35:28 +0900

    save

--- sdk/src/packet.rs
+++ sdk/src/packet.rs
@@ -39,6 +39,52 @@ pub struct Meta {
     pub sender_stake: u64,
 }
 
+mod serde_bytes_array {
+use core::convert::TryInto;
+
+use serde::de::Error;
+use serde::{Deserializer, Serializer};
+
+pub(crate) fn serialize<S>(bytes: &[u8], serializer: S) ->
+Result<S::Ok, S::Error>
+where
+S: Serializer,
+{
+serde_bytes::serialize(bytes, serializer)
+}
+
+/// This takes the result of
+[`serde_bytes::deserialize`] from
+`[u8]` to `[u8; N]`.
+pub(crate) fn deserialize<'de, D,
+const N: usize>(deserializer: D) ->
+Result<[u8; N], D::Error>
+where
+D: Deserializer<'de>,
+{
+let slice:
+&[u8] =
+serde_bytes::deserialize(deserializer)?;
+let
+array:
+[u8;
+N]
+=
+slice.try_into().map_err(|_|
+{
+let
+expected
+=
+format!("[u8;
+{}]",
+N);
+D::Error::invalid_length(slice.len(),
+&expected.as_str())
+})?;
+Ok(array)
+}
+}
+
 #[derive(Clone, Eq, Serialize, Deserialize)]
 #[repr(C)]
 pub struct Packet {
