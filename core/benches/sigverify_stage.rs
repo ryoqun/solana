b0ef041b3e31d0dbf44402c42ee9bf1c3c3586ae
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 13:54:51 +0900

    save

--- core/benches/sigverify_stage.rs
+++ core/benches/sigverify_stage.rs
@@ -225,8 +225,8 @@ fn prepare_batches(discard_factor: i32) -> (Vec<PacketBatch>, usize) {
 
 fn bench_shrink_sigverify_stage_core(bencher: &mut Bencher, discard_factor: i32) {
     let (batches0, num_valid_packets) = prepare_batches(discard_factor);
-    let (_verified_s, _verified_r) = unbounded();
-    let verifier = TransactionSigVerifier::new(_verified_s);
+    let (verified_s, verified_r) = solana_core::banking_tracer::BankingTracer::channel_for_test();
+    let verifier = TransactionSigVerifier::new(verified_s);
 
     let mut c = 0;
     let mut total_shrink_time = 0;
