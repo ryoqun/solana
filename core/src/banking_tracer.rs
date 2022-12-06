a4d171d4a2c15ebb3f345bfc253cb22999383c70
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 09:48:34 +0900

    save

--- core/src/banking_tracer.rs
+++ core/src/banking_tracer.rs
@@ -72,7 +72,7 @@ impl BankingTracer {
 
     pub fn new_bank_start(&self, slot: Slot) {
         if let Some(trace_output) = self.trace_output {
-            trace_output.0.0.send(3).unwrap();
+            trace_output.0.0.send(TimedTracedEvent(0, TracedEvent::NewBankStart(slot))).unwrap();
         }
     }
 }
