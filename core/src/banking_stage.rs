2c1f907a8abd3bacf1c47cda56a167fa00e4aedf
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 13:09:04 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -2026,13 +2026,13 @@ mod tests {
                 Blockstore::open(ledger_path.path())
                     .expect("Expected to be able to open database ledger"),
             );
+            let (exit, poh_recorder, poh_service, _entry_receiever) =
+                create_test_recorder(&bank, &blockstore, None, None);
             let banking_tracer = BankingTracer::new(blockstore.banking_tracer_path(), true, exit.clone()).unwrap();
             let (verified_sender, verified_receiver) = banking_tracer.create_channel();
             let (gossip_verified_vote_sender, gossip_verified_vote_receiver) = banking_tracer.create_channel();
             let (tpu_vote_sender, tpu_vote_receiver) = banking_tracer.create_channel();
 
-            let (exit, poh_recorder, poh_service, _entry_receiever) =
-                create_test_recorder(&bank, &blockstore, None, None);
             let cluster_info = new_test_cluster_info(Node::new_localhost().info);
             let cluster_info = Arc::new(cluster_info);
             let (gossip_vote_sender, _gossip_vote_receiver) = unbounded();
