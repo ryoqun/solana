46176017ef3c161e02bd6a3767fa6d6444e83932
Ryo Onodera <ryoqun@gmail.com>
2022-12-06 13:37:26 +0900

    save

--- core/src/sigverify_stage.rs
+++ core/src/sigverify_stage.rs
@@ -558,7 +558,7 @@ mod tests {
                     .iter_mut()
                     .for_each(|packet| packet.meta.flags |= PacketFlags::TRACER_PACKET);
                 assert_eq!(batch.len(), packets_per_batch);
-                packet_s.send(vec![batch]).unwrap();
+                packet_s.send(Ovec![batch], None)).unwrap();
             }
         }
         let mut received = 0;
