47125b8011871ee3d31bfad60dd2d45c23cead33
Ryo Onodera <ryoqun@gmail.com>
2022-12-07 09:11:15 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -3361,7 +3361,7 @@ fn main() {
                     }
                 }
             }
-            ("recreate-blocks", Some(arg_matches)) => {
+            ("simulate-leader-block", Some(arg_matches)) => {
                 let mut accounts_index_config = AccountsIndexConfig::default();
                 if let Some(bins) = value_t!(arg_matches, "accounts_index_bins", usize).ok() {
                     accounts_index_config.bins = Some(bins);
