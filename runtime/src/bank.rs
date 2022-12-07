95c0240fc1f4ef661e43cf5ed79cb531b4c8a255
Ryo Onodera <ryoqun@gmail.com>
2022-12-07 12:28:26 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4467,7 +4467,7 @@ impl Bank {
             &self.feature_set,
             &self.fee_structure,
             account_overrides,
-            self.runtime_config.is_check_age_skipped().then(Some(self.get_lamports_per_signature())),
+            self.runtime_config.is_check_age_skipped().then(|| Some(self.get_lamports_per_signature())),
         );
         load_time.stop();
 
