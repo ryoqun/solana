e0c0200580fd664a7758241e015540b7d927fbc0
Ryo Onodera <ryoqun@gmail.com>
2022-12-07 21:36:31 +0900

    save

--- core/benches/banking_trace.rs
+++ core/benches/banking_trace.rs
@@ -92,3 +92,37 @@ fn bench_banking_tracer_sustained_throughput(bencher: &mut Bencher) {
         s.send(m.clone()).unwrap();
     });
 }
+
+#[bench]
+fn bench_banking_tracer_10_1gb(bencher: &mut Bencher) {
+    let exit = std::sync::Arc::<std::sync::atomic::AtomicBool>::default();
+
+    let len = 4;
+    let chunk_size = 10;
+    let tx = test_tx();
+    let aa = to_packet_batches(&vec![tx; len], chunk_size);
+    let m = Arc::new((aa.clone(), None));
+
+    bencher.iter(move || {
+        let tracer = solana_core::banking_trace::BankingTracer::_new(
+            std::path::PathBuf::new().join("/tmp/banking-tracer"),
+            true,
+            exit.clone(),
+            1024 * 1024 * 1024,
+        )
+        .unwrap();
+        let (s, r) = tracer.create_channel_non_vote();
+
+        let t = std::thread::spawn(move || {
+            solana_core::banking_trace::sender_overhead_minimized_loop(exit.clone(), r, |m| {
+                test::black_box(m);
+            })
+        });
+
+        for _ in 0..1_000_000 {
+            s.send(m.clone()).unwrap();
+        }
+        drop(r);
+        t.join();
+    });
+}
