bdabf11a3b6a9bf023b38e05b98b14bccb1c72b2
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 15:23:08 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -99,10 +99,10 @@ impl PacketDeserializer {
 
         let mut num_packets_received: usize = packet_batches.iter().map(|batch| batch.len()).sum();
         while let Ok(a) = self.packet_batch_receiver.try_recv() {
-            let (packet_batch, tracer_packet_stats_option) = (&a.0, &a.1);
+            let (_packet_batch, tracer_packet_stats_option) = (&a.0, &a.1);
             trace!("got more packet batches in packet deserializer");
             let (packets_received, packet_count_overflowed) = num_packets_received
-                .overflowing_add(packet_batch.iter().map(|batch| batch.len()).sum());
+                .overflowing_add(a.0.iter().map(|batch| batch.len()).sum());
 
             if let Some(tracer_packet_stats) = &tracer_packet_stats_option {
                 if let Some(aggregated_tracer_packet_stats) =
