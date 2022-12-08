7e02880af123cf50806dc5257417e92ae92aa492
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 10:28:52 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -2031,8 +2031,7 @@ mod tests {
             );
             let (exit, poh_recorder, poh_service, _entry_receiever) =
                 create_test_recorder(&bank, &blockstore, None, None);
-            let banking_tracer =
-                BankingTracer::new(blockstore.banking_tracer_path(), true, exit.clone()).unwrap();
+            let banking_tracer = BankingTracer::new_disabled();
             let (verified_sender, verified_receiver) = banking_tracer.create_channel_non_vote();
             let (gossip_verified_vote_sender, gossip_verified_vote_receiver) =
                 banking_tracer.create_channel_gossip_vote();
