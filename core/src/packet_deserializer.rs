9813ad4284ff769762824a8947f3c86f9cbc122f
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 21:20:16 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -51,7 +51,7 @@ impl PacketDeserializer {
     }
 
     fn on_each_pachet_batch(banking_batch: &[BankingPacketBatch], mut on_each: impl FnMut(&PacketBatch)) {
-        for (batch_vec, _) in *banking_batch.iter().as_ref() {
+        for (batch_vec, _) in banking_batch.iter().deref() {
             for batch in &batch_vec {
                 on_each(&batch)
             }
