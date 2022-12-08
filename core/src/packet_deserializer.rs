4ac13d95a4709b2e5a0207d873f178221a056edb
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 14:15:51 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -58,13 +58,13 @@ impl PacketDeserializer {
         let mut passed_sigverify_count: usize = 0;
         let mut failed_sigverify_count: usize = 0;
         let mut deserialized_packets = Vec::with_capacity(packet_count);
-        for &packet_batch in *packet_batches {
-            let packet_indexes = Self::generate_packet_indexes(packet_batch);
+        for packet_batch in *packet_batches {
+            let packet_indexes = Self::generate_packet_indexes(&packet_batch);
 
             passed_sigverify_count += packet_indexes.len();
             failed_sigverify_count += packet_batch.len().saturating_sub(packet_indexes.len());
 
-            deserialized_packets.extend(Self::deserialize_packets(packet_batch, &packet_indexes));
+            deserialized_packets.extend(Self::deserialize_packets(&packet_batch, &packet_indexes));
         }
 
         ReceivePacketResults {
