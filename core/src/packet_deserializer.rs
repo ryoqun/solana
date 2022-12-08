4470d93a03d22017dc3f11cf821d6a491129bb12
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 15:04:19 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -122,7 +122,7 @@ impl PacketDeserializer {
         impl<'bbb> std::iter::Iterator for AA<'bbb> {
             type Item = &'bbb PacketBatch;
 
-            fn next(&mut self) -> Option<Self::Item> {
+            fn next(&'bbb mut self) -> Option<Self::Item> {
                 let mut found = None;
                 loop {
                     if let Some(message) = self.messages.get(self.i) {
