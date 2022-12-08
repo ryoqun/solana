26c389b3968740e51a9532bbf094c4e01a9db5d3
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 21:13:30 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -44,7 +44,7 @@ impl PacketDeserializer {
         let (num_packets_received, packet_batches, sigverify_tracer_stats_option) =
             self.receive_until(recv_timeout, capacity)?;
         Ok(Self::deserialize_and_collect_packets(
-            num_packets_received
+            num_packets_received,
             &packet_batches,
             sigverify_tracer_stats_option,
         ))
