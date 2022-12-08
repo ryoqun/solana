ccf3b12ab0e7ff81152b61163124be6d489fc364
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 14:20:07 +0900

    save

--- core/src/packet_deserializer.rs
+++ core/src/packet_deserializer.rs
@@ -85,10 +85,12 @@ impl PacketDeserializer {
         let a = self.packet_batch_receiver.recv_timeout(recv_timeout)?;
         let (mut packet_batches, mut aggregated_tracer_packet_stats_option) =
             (a.0.clone(), a.1.clone());
+        let mut messages = vec![a];
 
         let mut num_packets_received: usize = packet_batches.iter().map(|batch| batch.len()).sum();
         while let Ok(a) = self.packet_batch_receiver.try_recv() {
             let (packet_batch, tracer_packet_stats_option) = (&a.0, &a.1);
+            messages.push(a);
             trace!("got more packet batches in packet deserializer");
             let (packets_received, packet_count_overflowed) = num_packets_received
                 .overflowing_add(packet_batch.iter().map(|batch| batch.len()).sum());
