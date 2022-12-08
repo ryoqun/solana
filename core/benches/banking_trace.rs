d1b96c9a2f5a3c7d83653695e54042d25795b271
Ryo Onodera <ryoqun@gmail.com>
2022-12-08 09:15:26 +0900

    save

--- core/benches/banking_trace.rs
+++ core/benches/banking_trace.rs
@@ -104,7 +104,7 @@ fn bench_banking_tracer_background_thread_throughput(bencher: &mut Bencher) {
     bencher.iter(move || {
         let exit = std::sync::Arc::<std::sync::atomic::AtomicBool>::default();
 
-        let dir_cleanup = std::fs::remove_dir_all("/tmp/banking-trace/").map_err(|err| {
+        let dir_cleanup = std::fs::remove_dir_all("/tmp/banking-trace/").or(|err| {
             if err.kind() == std::io::ErrorKind::NotFound {
                 Ok(())
             } else {
