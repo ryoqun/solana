649f31fea808c6c27ed4f42b9c7ce062f555fc8f
Ryo Onodera <ryoqun@gmail.com>
2022-12-09 22:31:57 +0900

    save

--- core/benches/sigverify_stage.rs
+++ core/benches/sigverify_stage.rs
@@ -175,7 +175,7 @@ fn bench_sigverify_stage(bencher: &mut Bencher) {
         loop {
             if let Ok(message) = verified_r.recv_timeout(Duration::from_millis(10)) {
                 let (verifieds, _) = &*message;
-                for v in verifieds.iter().rev() {
+                for v: usize in verifieds.iter().rev() {
                     received += v.len();
                     batches.push(v.clone());
                 }
