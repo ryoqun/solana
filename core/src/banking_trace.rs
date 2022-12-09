c32d58bbb32c671b37442842f1a4869519929998
Ryo Onodera <ryoqun@gmail.com>
2022-12-09 12:44:12 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -131,14 +131,14 @@ pub fn sender_overhead_minimized_receiver_loop<T, U: Default, E, const SLEEP_MS:
     receiver: Receiver<T>,
     mut on_recv: impl FnMut(T) -> Result<U, E>,
 ) -> Result<U, E> {
-    let mut value = None;
+    let mut last_value = None;
 
     'outer: while !exit.load(Ordering::Relaxed) {
         'inner: loop {
             // avoid futex-based blocking here, otherwise a sender would have to
             // wake me up at a syscall cost...
             match receiver.try_recv() {
-                Ok(message) => value = Some(on_recv(message)?),
+                Ok(message) => last_value = Some(on_recv(message)?),
                 Err(TryRecvError::Empty) => break 'inner,
                 Err(TryRecvError::Disconnected) => {
                     assert_eq!(receiver.len(), 0);
@@ -149,7 +149,7 @@ pub fn sender_overhead_minimized_receiver_loop<T, U: Default, E, const SLEEP_MS:
         sleep(Duration::from_millis(SLEEP_MS));
     }
 
-    Ok(value.unwrap_or_default())
+    Ok(last_value.unwrap_or_default())
 }
 
 impl BankingTracer {
