d0210de1061d06f55aa1dc497b8203607e46d902
Ryo Onodera <ryoqun@gmail.com>
2022-12-09 12:08:06 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -1920,7 +1920,9 @@ impl BankingStage {
             bank_thread_hdl.join()?;
         }
         if let Some(tracer_thread_hdl) = self.tracer_thread_hdl {
-            tracer_thread_hdl.join()?.unwrap();
+            if let Err(tracer_result) = tracer_thread_hdl.join()? {
+                error!("tracer thread error: {:?}", tracer_thread);
+            }
         }
         Ok(())
     }
