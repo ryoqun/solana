689e8b84f2864d222ee98d38c6070691d783d578
Ryo Onodera <ryoqun@gmail.com>
2022-12-12 12:56:07 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -607,7 +607,7 @@ impl BankingTraceReplayer {
                 (None, packet_batches_by_time.range(..))
             };
             info!(
-                "replaying events: {} out of {} at {} (adjust to {:?})",
+                "replaying banking trace events: {} out of {}, starting at slot {} (adjusted to {:?})",
                 range_iter.clone().count(),
                 packet_batches_by_time.len(),
                 bank_slot,
@@ -660,7 +660,10 @@ impl BankingTraceReplayer {
             banking_tracer,
         );
 
-        bank.skip_check_age();
+        if std::env::var("SKIP_CHECK_AGE").is_ok() {
+            warn!("skipping check age as requested....");
+            bank.skip_check_age();
+        }
 
         bank.clear_signatures();
         poh_recorder.write().unwrap().set_bank(&bank, false);
