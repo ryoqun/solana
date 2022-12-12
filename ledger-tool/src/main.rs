5e60f9adc0adf52f324e604b580dbd0cd81c4bd5
Ryo Onodera <ryoqun@gmail.com>
2022-12-12 21:52:55 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -3371,7 +3371,7 @@ fn main() {
                 let simulator = BankingSimulator::new(PathBuf::new().join("/dev/stdin"));
 
                 if std::env::var("DUMP").is_ok() {
-                    simulator.dump();
+                    simulator.dump(None);
                     return
                 }
 
