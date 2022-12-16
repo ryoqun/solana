3fa173b9f7d5d74de7e27874ddc09b0e1c2ecacf
Ryo Onodera <ryoqun@gmail.com>
2022-12-16 15:43:44 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -4,7 +4,7 @@
 
 use {
     crate::{
-        banking_trace::{BankingPacketReceiver, BankingTracer, TracerThread, TracerThreadResult},
+        banking_trace::{BankingPacketReceiver, BankingTracer, TracerThread},
         forward_packet_batches_by_accounts::ForwardPacketBatchesByAccounts,
         immutable_deserialized_packet::ImmutableDeserializedPacket,
         latest_unprocessed_votes::{LatestUnprocessedVotes, VoteSource},
