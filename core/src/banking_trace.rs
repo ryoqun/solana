a7fc508b83c97fa5439c927b61c63a537aa74c1e
Ryo Onodera <ryoqun@gmail.com>
2022-12-16 15:51:03 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -389,7 +389,7 @@ pub mod for_test {
     }
 
     pub fn terminate_tracer(
-        tracer: impl AsRef<BankingTracer>,
+        tracer: Arc<BankingTracer>,
         main_thread: JoinHandle<TracerThreadResult>,
         sender: TracedSender,
         exit: Option<Arc<AtomicBool>>,
