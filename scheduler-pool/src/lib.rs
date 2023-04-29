2462392c549cf05e54d24b5ef6ee685d90ecd88d
Ryo Onodera <ryoqun@gmail.com>
2023-04-29 14:22:08 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -273,7 +273,8 @@ impl<T: TransactionHandler> PooledScheduler2<T> {
             std::thread::spawn({
                 let pool = pool.clone();
                 move || {
-                    while let Ok((tx, idx, _b)) = transaction_receiver.recv() {
+                    while let Ok((tx, idx, b)) = transaction_receiver.recv() {
+                        std::hint::black_box(b));
                         let mut result = Ok(());
                         let mut timings = Default::default();
                         T::handle_transaction(&mut result, &mut timings, &bank, &tx, idx, &pool);
