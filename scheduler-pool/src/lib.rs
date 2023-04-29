5a257bf55fc82e0b5223f85acaed8a457bad7a5d
Ryo Onodera <ryoqun@gmail.com>
2023-04-29 15:40:37 +0900

    save

--- scheduler-pool/src/lib.rs
+++ scheduler-pool/src/lib.rs
@@ -273,7 +273,7 @@ impl<T: TransactionHandler> PooledScheduler2<T> {
         let (transaction_sender, transaction_receiver) = crossbeam_channel::unbounded();
         let (result_sender, result_receiver) = crossbeam_channel::unbounded();
 
-        for _ in 0..8 {
+        for _ in 0..4 {
             let bank = Arc::clone(initial_context.bank());
             let transaction_receiver = transaction_receiver.clone();
             let result_sender = result_sender.clone();
