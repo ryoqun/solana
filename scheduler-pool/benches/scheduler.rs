6bd70886790e38285ccdd7e1e5370ddd5c105be8
Ryo Onodera <ryoqun@gmail.com>
2023-04-29 15:39:36 +0900

    save

--- scheduler-pool/benches/scheduler.rs
+++ scheduler-pool/benches/scheduler.rs
@@ -52,6 +52,27 @@ impl solana_scheduler_pool::TransactionHandler for D {
     }
 }
 
+struct D2;
+
+impl solana_scheduler_pool::TransactionHandler for D2 {
+    fn handle_transaction(
+        _result: &mut solana_sdk::transaction::Result<()>,
+        _timings: &mut ExecuteTimings,
+        bank: &Arc<Bank>,
+        transaction: &SanitizedTransaction,
+        _index: usize,
+        _pool: &SchedulerPool,
+    ) {
+        //std::hint::black_box(bank.clone());
+        let mut i = 0;
+        for _ in 1..10 {
+            std::hint::black_box((Arc::downgrade(bank)).upgrade().unwrap());
+            i += bank.get_fee_for_message_with_lamports_per_signature(transaction.message(), 23)
+        }
+        std::hint::black_box(i);
+    }
+}
+
 #[bench]
 fn bench_pooled_scheduler1(bencher: &mut Bencher) {
     solana_logger::setup();
@@ -101,6 +122,43 @@ fn bench_pooled_scheduler2(bencher: &mut Bencher) {
     let create_context = || SchedulingContext::new(SchedulingMode::BlockVerification, bank.clone());
 
     let mut scheduler = PooledScheduler2::<D>::spawn(pool, create_context());
+    let tx0 = &SanitizedTransaction::from_transaction_for_tests(system_transaction::transfer(
+        &mint_keypair,
+        &solana_sdk::pubkey::new_rand(),
+        2,
+        genesis_config.hash(),
+    ));
+    bencher.iter(|| {
+        let tx_count = 20_000;
+
+        for _ in 0..tx_count {
+            scheduler.schedule_execution(tx0.clone(), 0);
+        }
+        assert_matches!(
+            scheduler.wait_for_termination(&WaitReason::TerminatedToFreeze),
+            Some((Ok(()), _))
+        );
+        scheduler.replace_context(create_context());
+        let t = scheduler.recv(tx_count);
+        t.join().unwrap();
+    });
+}
+
+#[bench]
+fn bench_pooled_scheduler3(bencher: &mut Bencher) {
+    solana_logger::setup();
+
+    let GenesisConfigInfo {
+        genesis_config,
+        mint_keypair,
+        ..
+    } = create_genesis_config(1_000_000_000);
+    let bank = &Arc::new(Bank::new_for_tests(&genesis_config));
+    let _ignored_prioritization_fee_cache = Arc::new(PrioritizationFeeCache::new(0u64));
+    let pool = SchedulerPool::new(None, None, None, _ignored_prioritization_fee_cache);
+    let create_context = || SchedulingContext::new(SchedulingMode::BlockVerification, bank.clone());
+
+    let mut scheduler = PooledScheduler2::<D2>::spawn(pool, create_context());
     let tx0 = &SanitizedTransaction::from_transaction_for_tests(system_transaction::transfer(
         &mint_keypair,
         &solana_sdk::pubkey::new_rand(),
