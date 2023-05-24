3f5139ec2d74ccb4b41e8ec62f0d82d6a7316719
Ryo Onodera <ryoqun@gmail.com>
2023-05-24 15:00:27 +0900

    save

--- scheduler-pool/benches/scheduler.rs
+++ scheduler-pool/benches/scheduler.rs
@@ -455,7 +455,7 @@ mod nonblocking {
             let pool = SchedulerPool::new(None, None, None, _ignored_prioritization_fee_cache);
             let context = SchedulingContext::new(SchedulingMode::BlockVerification, bank.clone());
             let mut scheduler = NonblockingScheduler::<SleepyHandler>::spawn(pool, context.clone(), 4);
-            let scenario = &vec![
+            let scenario: &Vec<Step> = &vec![
                 Step::Batch((0..20).map(tx_with_index).collect()),
                 Step::Synchronize,
                 Step::Batch((0..20).map(tx_with_index).collect()),
