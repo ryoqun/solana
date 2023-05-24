add330b4435fcbdbe159b184290286ced9e3e279
Ryo Onodera <ryoqun@gmail.com>
2023-05-24 16:35:16 +0900

    save

--- scheduler-pool/benches/scheduler.rs
+++ scheduler-pool/benches/scheduler.rs
@@ -48,7 +48,7 @@ impl ScheduleExecutionArg for ScheduleExecutionArgForBench {
     type TransactionWithIndex<'_tx> = TransactionWithIndexForBench;
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Clone)]
 struct BenchFriendlyHandler<SEA: ScheduleExecutionArg, const MUTATE_ARC: bool>(
     std::marker::PhantomData<SEA>,
 );
