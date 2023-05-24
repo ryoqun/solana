8931a792237b8b731e68648c05929302dcd0eaae
Ryo Onodera <ryoqun@gmail.com>
2023-05-24 23:15:04 +0900

    save

--- scheduler-pool/benches/scheduler.rs
+++ scheduler-pool/benches/scheduler.rs
@@ -795,12 +795,12 @@ fn execute_batches(
         }
 
         #[bench]
-        fn bench_txes_with_long_serialized_runs_with_interleaved_synchronization(bencher: &mut Bencher, synchronize: bool) {
+        fn bench_txes_with_long_serialized_runs_with_interleaved_synchronization(bencher: &mut Bencher) {
             bench_txes_with_long_serialized_runs(bencher, true);
         }
 
         #[bench]
-        fn bench_txes_with_long_serialized_runs_without_interleaved_synchronization(bencher: &mut Bencher, synchronize: bool) {
+        fn bench_txes_with_long_serialized_runs_without_interleaved_synchronization(bencher: &mut Bencher) {
             bench_txes_with_long_serialized_runs(bencher, false);
         }
     }
