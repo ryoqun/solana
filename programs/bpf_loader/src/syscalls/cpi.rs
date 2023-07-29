bcefcf01859653b10477467deb6e34327dee7608
Ryo Onodera <ryoqun@gmail.com>
2023-07-29 04:06:44 +0000

    save 2023y07m29d04h06m44s459445828ns

--- programs/bpf_loader/src/syscalls/cpi.rs
+++ programs/bpf_loader/src/syscalls/cpi.rs
@@ -1420,7 +1420,7 @@ fn update_caller_account(
                     prev_len
                 }
                 .saturating_sub(post_len);
-                log::warn!("spare: {:?}", unsafe { std::mem::transmute::<_, Option<&[u8]>>(callee_account.spare_data_capacity_mut()?.get(..50))});
+                log::warn!("spare1: {:?x}", unsafe { std::mem::transmute::<_, Option<&[u8]>>(callee_account.spare_data_capacity_mut()?.get(..50))});
                 if spare_len > 0 {
                     let dst = callee_account
                         .spare_data_capacity_mut()?
@@ -1430,6 +1430,7 @@ fn update_caller_account(
                     // Safety: we check bounds above
                     unsafe { ptr::write_bytes(dst, 0, spare_len) };
                 }
+                log::warn!("spare2: {:?x}", unsafe { std::mem::transmute::<_, Option<&[u8]>>(callee_account.spare_data_capacity_mut()?.get(..50))});
 
                 // Here we zero the realloc region.
                 //
