c0966f8620942eec1afebef4df492f8555d61f2b
Ryo Onodera <ryoqun@gmail.com>
2023-07-29 05:12:04 +0000

    save 2023y07m29d05h12m04s554536668ns

--- programs/sbf/rust/invoke/src/processor.rs
+++ programs/sbf/rust/invoke/src/processor.rs
@@ -907,12 +907,14 @@ fn process_instruction(
             }
             */
             msg!("ptr: {:p}", &rc_box);
+            /*
             unsafe {
                 std::ptr::write(
                     &account.data as *const _ as usize as *mut Rc<RefCell<&mut [u8]>>,
                     Rc::from_raw(((&rc_box as *const _) as usize + mem::size_of::<usize>() * 2) as *mut _),
                 );
             }
+            */
             unsafe {
                 std::ptr::write(
                     &target_account.data as *const _ as usize as *mut Rc<RefCell<&mut [u8]>>,
