4c06ced29bcb5ec2ececd44a8b5c61e66d836989
Ryo Onodera <ryoqun@gmail.com>
2023-07-29 04:12:44 +0000

    save 2023y07m29d04h12m44s133806986ns

--- programs/sbf/rust/invoke/src/processor.rs
+++ programs/sbf/rust/invoke/src/processor.rs
@@ -949,7 +949,7 @@ fn process_instruction(
             )
             .unwrap();
             msg!("CPI SUCCESS!");
-            msg!("spare2: {:x?}", unsafe { slice::from_raw_parts(account.data.borrow().as_ptr()) });
+            msg!("spare2: {:x?}", unsafe { slice::from_raw_parts(account.data.borrow().as_ptr(), 100) });
 
             unsafe { *serialized_len_ptr = 42 as u64 };
             unsafe { *serialized_len_ptr2 = rc_box_size as u64 /*+ 1*/};
