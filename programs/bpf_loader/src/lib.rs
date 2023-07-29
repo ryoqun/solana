70f21404b4909e9e629b57d1b3ba1ec750b40058
Ryo Onodera <ryoqun@gmail.com>
2023-07-29 03:20:47 +0000

    save 2023y07m29d03h20m47s900064623ns

--- programs/bpf_loader/src/lib.rs
+++ programs/bpf_loader/src/lib.rs
@@ -229,7 +229,7 @@ pub fn create_vm<'a, 'b>(
                 .map_err(|_| ())?;
 
             use solana_sdk::account::ReadableAccount;
-            log::warn!("cow {index_in_transaction} is_shared: {} len: {}, capacity: {}", account.is_shared(), account.data().len(), account.data().capacity());
+            log::warn!("cow {index_in_transaction} is_shared: {} len: {}, capacity: {}", account.is_shared(), account.data().len(), account.capacity());
             if account.is_shared() {
                 // See BorrowedAccount::make_data_mut() as to why we reserve extra
                 // MAX_PERMITTED_DATA_INCREASE bytes here.
