a7bcb93d7a785a90ddbc1f373193b0fce4e1d041
Ryo Onodera <ryoqun@gmail.com>
2024-01-11 14:38:38 +0000

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5200,7 +5200,7 @@ impl Bank {
 
             // load transactions
             use crate::accounts::load_transaction_accounts;
-            let loaded_transaction = match load_transaction_accounts(
+            let mut loaded_transaction = match load_transaction_accounts(
                 &self.rc.accounts.accounts_db,
                 &self.ancestors,
                 sanitized_tx,
