a50e8fa7c0753855d070bbc9f614e9a943f3796c
Ryo Onodera <ryoqun@gmail.com>
2024-01-11 14:23:18 +0000

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -5352,6 +5352,7 @@ impl Bank {
             };
 
             // load transactions
+            use crate::accounts::load_transaction_accounts;
             let loaded_transaction = match load_transaction_accounts(
                 self.rc.accounts.accounts_db,
                 self.ancestors,
