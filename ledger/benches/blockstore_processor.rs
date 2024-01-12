07ff2f0a6aadafae390800bb066a5880ec7e5100
Ryo Onodera <ryoqun@gmail.com>
2024-01-12 05:48:12 +0000

    save

--- ledger/benches/blockstore_processor.rs
+++ ledger/benches/blockstore_processor.rs
@@ -180,8 +180,8 @@ fn bench_execute_batch2(
     eprintln!("profile me!: {}", std::process::id());
     std::thread::sleep(std::time::Duration::from_secs(10));
 
-    std::thread::scoped(move |scope| {
-        scope::spawn(|| {
+    std::thread::scope(move |scope| {
+        scope.spawn(|| {
         for _ in 0..100 {
             for _ in 0..(64/batch_size) {
                 let batch = batches_iter.next().unwrap();
