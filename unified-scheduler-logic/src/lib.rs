5d446917900f448abb7ac3d65f411ed50502c703
Ryo Onodera <ryoqun@gmail.com>
2024-02-26 22:05:54 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -619,8 +619,8 @@ impl SchedulingStateMachine {
         } else {
             MyRc::update_strong_count(task_ptr, i);
             //MyRc::decrement_strong_count(task_ptr);
-            mem::forget(t);
-            //drop(t);
+            //mem::forget(t);
+            drop(t);
             None
         }
     } }
