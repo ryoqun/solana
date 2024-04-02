2c370a69b97e0b66ccec65080e9186e40318cd96
Ryo Onodera <ryoqun@gmail.com>
2024-04-02 17:47:02 +0900

    Move to Dockerfile

--- ci/test-miri.sh
+++ ci/test-miri.sh
@@ -4,7 +4,5 @@ set -eo pipefail
 
 source ci/rust-version.sh nightly
 
-rustup component add miri --toolchain "$rust_nightly"
-
 # miri is very slow; so only run very few of selective tests!
 cargo "+${rust_nightly}" miri test -p solana-program -- hash:: account_info::
