fd16ae438649bddfad150029205575c4d7d3029f
Ryo Onodera <ryoqun@gmail.com>
2024-04-02 17:26:31 +0900

    fmt

--- ci/test-miri.sh
+++ ci/test-miri.sh
@@ -8,4 +8,3 @@ rustup component add miri --toolchain "$rust_nightly"
 
 # miri is very slow; so only run very few of selective tests!
 cargo "+${rust_nightly}" miri test -p solana-program -- hash:: account_info::
-
