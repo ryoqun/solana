50ddb5c64b9ab54311ec6974d7004291a4f7dd2b
Ryo Onodera <ryoqun@gmail.com>
2024-04-02 21:41:08 +0900

    Test solana-unified-scheduler-logic under miri

--- ci/test-miri.sh
+++ ci/test-miri.sh
@@ -6,3 +6,4 @@ source ci/rust-version.sh nightly
 
 # miri is very slow; so only run very few of selective tests!
 cargo "+${rust_nightly}" miri test -p solana-program -- hash:: account_info::
+cargo "+${rust_nightly}" miri test -p solana-unified-scheduler-logic
