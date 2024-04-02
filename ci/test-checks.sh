5a235cc760137f81f40dd49c13f2c31d28b3a514
Ryo Onodera <ryoqun@gmail.com>
2024-04-02 17:07:28 +0900

    fail fast...

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -46,6 +46,9 @@ echo --- build environment
   wasm-pack --version
 )
 
+rustup component add miri --toolchain "$rust_stable"
+cargo miri test -p solana-program -- hash:: account_info::
+
 export RUST_BACKTRACE=1
 export RUSTFLAGS="-D warnings -A incomplete_features"
 
