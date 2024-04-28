c08bc7bfa5c1357aaf0b5c86945a51e63b4a6847
Ryo Onodera <ryoqun@gmail.com>
2024-04-28 23:14:12 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -528,7 +528,8 @@ where
     TH: TaskHandler,
 {
     fn is_trashed(&self) -> bool {
-        self.thread_manager.read().unwrap().is_aborted()
+        self.usage_queue_loader.usage_queue_count() > 200_000 ||
+            self.thread_manager.read().unwrap().is_aborted()
     }
 }
 
