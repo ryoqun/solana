af8db0c46e9797722fd563b8780c60b2c1bffe61
Ryo Onodera <ryoqun@gmail.com>
2024-04-28 15:57:31 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -120,6 +120,16 @@ where
                 let scheduler_pool = scheduler_pool_receiver.into_iter().next().unwrap();
                 loop {
                     sleep(Duration::from_secs(10));
+                    let inners = scheduler_pool.scheduler_inners.lock().unwrap();
+                    let unused_schedulers = vec![];
+                    let mut i = 0;
+                    while i < inners.len() {
+                        if true {
+                            unused_schedulers.push(inners.remove(i));
+                        } else {
+                            i += 1
+                        }
+                    }
                 }
             }
         };
