6eaa19a07ec21d1698f199539edcc4657db51671
Ryo Onodera <ryoqun@gmail.com>
2024-04-30 14:45:47 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -202,7 +202,7 @@ where
                         inners.filter(|(_inner, pooled_at)| {
                             now.duration_since(*pooled_at) <= max_pooling_duration
                         });
-                        scheduler_inners.extend(inners.collect:<Vec>());
+                        scheduler_inners.extend(inners.collect::<Vec>());
                         let new_inner_count = scheduler_inners.len();
 
                         old_inner_count
