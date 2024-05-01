e478e0d2e5baf26ae7cc4b82496c3697bd4204ed
Ryo Onodera <ryoqun@gmail.com>
2024-05-01 23:08:10 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -978,7 +978,10 @@ where
                         // to measure _actual_ cpu usage easily with the select approach.
                         select! {
                             recv(finished_blocked_task_receiver) -> executed_task => {
-                                let executed_task = executed_task.expect("alive handler");
+                                let Ok(executed_task) = executed_task.expect("alive handler") else {
+                                    session_result_sender.send(result_with_timings).expect("always outlived receiver");
+                                    return;
+                                };
 
                                 state_machine.deschedule_task(&executed_task.task);
                                 if Self::accumulate_result_with_timings(&mut result_with_timings, executed_task) {
