2596fc71abf418ad067b5381675c4468de233ad6
Ryo Onodera <ryoqun@gmail.com>
2024-05-02 16:05:40 +0900

    save

--- runtime/src/installed_scheduler_pool.rs
+++ runtime/src/installed_scheduler_pool.rs
@@ -671,7 +671,7 @@ mod tests {
                 mocked
                     .expect_recover_error_after_abort()
                     .times(1)
-                    .returning(|| Err(TransactionError::InsufficientFundsForFee));
+                    .returning(|| TransactionError::InsufficientFundsForFee);
             }),
         );
 
