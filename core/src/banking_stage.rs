0da90ef34e0b0c7db4b31deaf537ff6fa8237ccf
Ryo Onodera <ryoqun@gmail.com>
2024-06-29 22:31:10 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -658,6 +658,7 @@ impl BankingStage {
         let decision_maker = DecisionMaker::new(cluster_info.id(), poh_recorder.clone());
 
         for receiver in [non_vote_receiver, tpu_vote_receiver, gossip_vote_receiver] {
+            let decision_maker = decision_maker.clone();
             let id_generator = id_generator.clone();
             let bank_forks = bank_forks.clone();
             std::thread::spawn(move || {
