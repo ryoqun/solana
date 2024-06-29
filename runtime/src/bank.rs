dc1672f7229df72d86889cf87757fcae99f471f1
Ryo Onodera <ryoqun@gmail.com>
2024-06-29 21:08:10 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -4913,7 +4913,7 @@ impl Bank {
                 ..
             },
             ..,
-        )) =  { self.load_execute_and_commit_transactions(
+        )) =  self.load_execute_and_commit_transactions(
             &batch,
             MAX_PROCESSING_AGE,
             false, // collect_balances
@@ -4925,7 +4925,7 @@ impl Bank {
             &mut ExecuteTimings::default(),
             Some(1000 * 1000),
             || {},
-        ) }  else { panic!() };
+        ) else { panic!() };
 
         execution_results.remove(0)
     }
