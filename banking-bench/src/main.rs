13af9bb89bb3674a8a5c06b151748102457439e6
Ryo Onodera <ryoqun@gmail.com>
2024-06-30 14:26:02 +0900

    save

--- banking-bench/src/main.rs
+++ banking-bench/src/main.rs
@@ -574,6 +574,9 @@ fn main() {
 
             let mut new_bank_time = Measure::start("new_bank");
             let new_slot = bank.slot() + 1;
+            if let Some((Err(error), _timings)) = bank.wait_for_completed_scheduler() {
+                error!("error is returned after waiting for completed scheduler...: {error:?}");
+            }
             let new_bank = Bank::new_from_parent(bank.clone(), &collector, new_slot);
             new_bank_time.stop();
 
