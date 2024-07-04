1883bba3bb466ffcbe8c6070078d263beebc4834
Ryo Onodera <ryoqun@gmail.com>
2024-07-05 00:36:20 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -832,7 +832,6 @@ impl SchedulingStateMachine {
                                     current_tasks.insert(new_task.index, new_task.clone());
                                     Ok(())
                                 } else {
-                                    //panic!("{:?} tt: {:?} new_c: {:?}", current_tasks.keys(), t.iter().map(|t| t.index).collect::<Vec<_>>(), &new_c);
                                     *current_usage = Usage::Readonly(new_c);
                                     Err(())
                                 };
