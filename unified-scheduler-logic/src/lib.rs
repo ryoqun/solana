7b58ad6e396e25c9208b307f74f297436be24252
Ryo Onodera <ryoqun@gmail.com>
2024-07-04 21:02:31 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1453,7 +1453,91 @@ mod tests {
     }
 
     #[test]
-    fn test_higher_priority_locking() {
+    fn test_higher_priority_locking_write_read() {
+        let conflicting_address1 = Pubkey::new_unique();
+        let conflicting_address2 = Pubkey::new_unique();
+        let sanitized1 =
+            transaction_with_writable_address2(conflicting_address1, conflicting_address2);
+        let sanitized2 =
+            transaction_with_writable_address2(conflicting_address1, conflicting_address2);
+        let sanitized0_1 = transaction_with_writable_address(conflicting_address1);
+        //let sanitized0_2 = transaction_with_writable_address(
+        let usage_queues = Rc::new(RefCell::new(HashMap::new()));
+        let address_loader = &mut create_address_loader(Some(usage_queues.clone()));
+        let task0_1 = SchedulingStateMachine::create_task(sanitized0_1, 50, address_loader);
+        //let task0_2 = SchedulingStateMachine::create_task(sanitized0_2, 51, address_loader);
+        let task1 = SchedulingStateMachine::create_task(sanitized1, 101, address_loader);
+        let task2 = SchedulingStateMachine::create_task(sanitized2, 99, address_loader);
+
+        let mut state_machine = unsafe {
+            SchedulingStateMachine::exclusively_initialize_current_thread_for_scheduling()
+        };
+
+        assert_matches!(
+            state_machine
+                .schedule_task(task0_1.clone())
+                .map(|t| t.task_index()),
+            Some(50)
+        );
+        // now
+        // addr1: locked by task_0_1, queue: []
+        // addr2: unlocked, queue: []
+
+        assert_matches!(state_machine.schedule_task(task1.clone()), None);
+        // now
+        // addr1: locked by task_0_1, queue: [task1]
+        // addr2: locked by task1, queue: []
+        //
+        assert_matches!(state_machine.schedule_task(task2.clone()), None);
+        // now
+        // addr1: locked by task_0_1, queue: [task2, task1]
+        // addr2: locked by task2, queue: [task1]
+
+        assert!(!state_machine.has_unblocked_task());
+        state_machine.deschedule_task(&task0_1);
+        assert!(state_machine.has_unblocked_task());
+        // now
+        // addr1: locked by task2, queue: [task1]
+        // addr2: locked by task2, queue: [task1]
+
+        assert_matches!(
+            state_machine
+                .schedule_next_unblocked_task()
+                .map(|t| t.task_index()),
+            Some(99)
+        );
+
+        state_machine.deschedule_task(&task2);
+        assert!(state_machine.has_unblocked_task());
+
+        assert_matches!(
+            state_machine
+                .schedule_next_unblocked_task()
+                .map(|t| t.task_index()),
+            Some(101)
+        );
+        state_machine.deschedule_task(&task1);
+
+        dbg!(state_machine);
+        // task1
+        //      blocked by addr1
+        //      locking addr2
+        // task2
+        //      locking addr1
+        //      blocked by addr2
+        //
+        /*
+        assert_matches!(
+            state_machine
+                .schedule_task(task0_2.clone())
+                .map(|t| t.task_index()),
+            Some(51)
+        );
+        assert_matches!(state_machine.schedule_task(task2.clone()), None);
+        */
+    }
+    #[test]
+    fn test_higher_priority_locking_write_write_and_read_read() {
         let conflicting_address1 = Pubkey::new_unique();
         let conflicting_address2 = Pubkey::new_unique();
         let sanitized1 =
