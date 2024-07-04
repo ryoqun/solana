277b41cc1eecc6e36095433576995a3958e633dd
Ryo Onodera <ryoqun@gmail.com>
2024-07-04 23:54:17 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -824,8 +824,8 @@ impl SchedulingStateMachine {
                                     }
                                 }
                                 if current_tasks.is_empty() {
-                                    //*current_usage = Usage::Writable;
-                                    //current_tasks.insert(new_task.index, new_task);
+                                    *current_usage = Usage::Writable;
+                                    current_tasks.insert(new_task.index, new_task);
                                 } else {
                                     //usage_queue.insert_blocked_usage_from_task(new_task.index, (RequestedUsage::Writable, new_task));
                                 }
