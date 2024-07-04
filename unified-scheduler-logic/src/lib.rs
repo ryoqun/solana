dbafb0257d4ed2f820cbf8259bf772318ea1f1cf
Ryo Onodera <ryoqun@gmail.com>
2024-07-04 22:17:28 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -781,7 +781,7 @@ impl SchedulingStateMachine {
                             },
                             (Usage::Readonly(count), RequestedUsage::Writable) => {
                                 //assert_eq!(count.current(), 1);
-                                let reverted_task = std::mem::replace(current_task, new_task.clone());
+                                let reverted_task = current_task.pop_first().unwrap();
                                 reverted_task.increment_blocked_usage_count(&mut self.count_token);
                                 *current_usage = Usage::Writable;
                                 usage_queue.insert_blocked_usage_from_task(reverted_task.index, (RequestedUsage::Readonly, reverted_task));
