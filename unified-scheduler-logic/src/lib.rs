3a5b34d12947bcc4dbd73d9044576ba7ef053a9b
Ryo Onodera <ryoqun@gmail.com>
2024-07-04 21:15:31 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1648,9 +1648,9 @@ mod tests {
         let conflicting_address1 = Pubkey::new_unique();
         let conflicting_address2 = Pubkey::new_unique();
         let sanitized1 =
-            transaction_with_writable_address2(conflicting_address1, conflicting_address2);
-        let sanitized2 =
             transaction_with_writable_read2(conflicting_address1, conflicting_address2);
+        let sanitized2 =
+            transaction_with_writable_address2(conflicting_address1, conflicting_address2);
         let sanitized0_1 = transaction_with_writable_address(conflicting_address1);
         //let sanitized0_2 = transaction_with_writable_address(
         let usage_queues = Rc::new(RefCell::new(HashMap::new()));
