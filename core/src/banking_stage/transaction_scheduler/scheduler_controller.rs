148c291073b0f669fc742172d52888767d4bb7be
Ryo Onodera <ryoqun@gmail.com>
2024-07-05 13:53:42 +0900

    save

--- core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
+++ core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
@@ -605,7 +605,7 @@ impl SchedulerController {
     /// from user input. They should never be zero.
     /// Any difference in the prioritization is negligible for
     /// the current transaction costs.
-    fn calculate_priority_and_cost(
+    pub(crate) fn calculate_priority_and_cost(
         transaction: &SanitizedTransaction,
         fee_budget_limits: &FeeBudgetLimits,
         bank: &Bank,
