813bdcbd7416ad2679d9a46d58c125a938bef36b
Ryo Onodera <ryoqun@gmail.com>
2024-07-05 14:11:33 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -709,7 +709,7 @@ impl BankingStage {
                                         };
 
                                         let (priority, cost) = SchedulerController::calculate_priority_and_cost(&tx, &fb.into(), &bank);
-                                        let i = (priority as u128) << 64 | task_ids[*i] as u128;
+                                        let i = ((u64::MAX - priority) as u128)) << 64 | task_ids[*i] as u128;
 
                                         Some((tx, i))
                                     })
