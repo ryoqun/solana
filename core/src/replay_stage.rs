23a0a607225c6eb9236d966b6f1736d2096acc31
Ryo Onodera <ryoqun@gmail.com>
2024-07-05 16:38:50 +0900

    save

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -2153,7 +2153,7 @@ impl ReplayStage {
             // new()-ing of its child bank
             banking_tracer.hash_event(parent.slot(), &parent.last_blockhash(), &parent.hash());
 
-            let tpu_bank = bank_forks.write().unwrap().insert(tpu_bank);
+            let tpu_bank = bank_forks.write().unwrap().insert(SchedulingMode::BlockProduction, tpu_bank);
             poh_recorder
                 .write()
                 .unwrap()
