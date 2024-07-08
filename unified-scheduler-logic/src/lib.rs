5a6951a0f3f5fc8666c49645bfa773f5281ee915
Ryo Onodera <ryoqun@gmail.com>
2024-07-08 15:49:49 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -818,7 +818,9 @@ impl SchedulingStateMachine {
                             (Usage::Writable, RequestedUsage::Writable) => {
                                 assert_eq!(1, current_tasks.len());
                                 let reverted_task = current_tasks.pop_first().unwrap().1;
-                                assert!(current_tasks.insert(new_task.index, new_task.clone()).is_none());
+                                assert!(current_tasks
+                                    .insert(new_task.index, new_task.clone())
+                                    .is_none());
                                 reverted_task.increment_blocked_usage_count(&mut self.count_token);
                                 usage_queue.insert_blocked_usage_from_task(
                                     reverted_task.index,
@@ -831,7 +833,9 @@ impl SchedulingStateMachine {
                                 assert_eq!(1, current_tasks.len());
                                 let reverted_task = current_tasks.pop_first().unwrap().1;
                                 // this assert(....is_none()) isn't tested...
-                                assert!(current_tasks.insert(new_task.index, new_task.clone()).is_none());
+                                assert!(current_tasks
+                                    .insert(new_task.index, new_task.clone())
+                                    .is_none());
                                 reverted_task.increment_blocked_usage_count(&mut self.count_token);
                                 *current_usage = Usage::Readonly(ShortCounter::one());
                                 usage_queue.insert_blocked_usage_from_task(
@@ -870,7 +874,9 @@ impl SchedulingStateMachine {
                                 }
                                 let r = if current_tasks.is_empty() {
                                     *current_usage = Usage::Writable;
-                                    assert!(current_tasks.insert(new_task.index, new_task.clone()).is_none());
+                                    assert!(current_tasks
+                                        .insert(new_task.index, new_task.clone())
+                                        .is_none());
                                     Ok(())
                                 } else {
                                     *current_usage = Usage::Readonly(new_c);
