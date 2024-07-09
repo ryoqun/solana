5456dc075b32f003d0a63c25e76e4b5a09517f89
Ryo Onodera <ryoqun@gmail.com>
2024-07-09 16:09:44 +0900

    save

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -104,7 +104,7 @@ pub const MAX_COMPLETED_SLOTS_IN_CHANNEL: usize = 100_000;
 // An upper bound on maximum number of data shreds we can handle in a slot
 // 32K shreds would allow ~320K peak TPS
 // (32K shreds per slot * 4 TX per shred * 2.5 slots per sec)
-pub const MAX_DATA_SHREDS_PER_SLOT: usize = 32_768;
+pub const MAX_DATA_SHREDS_PER_SLOT: usize = u32::MAX as usize;
 
 pub type CompletedSlotsSender = Sender<Vec<Slot>>;
 pub type CompletedSlotsReceiver = Receiver<Vec<Slot>>;
