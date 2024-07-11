07c2aa9dd72b41ba29af1601e8dd536ffb3f583e
Ryo Onodera <ryoqun@gmail.com>
2024-07-11 14:10:31 +0900

    save

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -3689,7 +3689,7 @@ impl Blockstore {
             let range_shreds = data_shreds.unwrap();
             let last_shred = range_shreds.last().unwrap();
             assert!(last_shred.data_complete() || last_shred.last_in_slot());
-            Shredder::deshred(range_shreds)
+            Shredder::deshred(&range_shreds)
                 .map_err(|e| {
                     BlockstoreError::InvalidShredData(Box::new(bincode::ErrorKind::Custom(
                         format!("could not reconstruct entries buffer from shreds: {e:?}"),
