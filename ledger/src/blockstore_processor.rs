be332e78e6289b570e64cb2b4dd38823b852730b
Ryo Onodera <ryoqun@gmail.com>
2024-07-12 09:45:14 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -1474,29 +1474,32 @@ pub fn confirm_slot(
 
     let mut current_entry = chunked_entries.next();
     let mut last_end_index: u32 = u32::MAX;
+    let mut all_entry = vec![];
+
     loop {
         let Some((entry, last_end_index)) = current_entry else {
             break;
         };
         let next_entry = chunked_entries.next();
         let is_full = next_entry.is_none() && slot_meta.is_full();
+        all_entry.append(entry)
 
-        confirm_slot_entries(
-            bank,
-            replay_tx_thread_pool,
-            (entry, is_full),
-            timing,
-            progress,
-            skip_verification,
-            transaction_status_sender,
-            entry_notification_sender,
-            replay_vote_sender,
-            recyclers,
-            log_messages_bytes_limit,
-            prioritization_fee_cache,
-        )?;
         current_entry = next_entry;
     }
+    confirm_slot_entries(
+        bank,
+        replay_tx_thread_pool,
+        (all_entry, is_full),
+        timing,
+        progress,
+        skip_verification,
+        transaction_status_sender,
+        entry_notification_sender,
+        replay_vote_sender,
+        recyclers,
+        log_messages_bytes_limit,
+        prioritization_fee_cache,
+    )?;
     if last_end_index != u32::MAX {
         progress.num_shreds = last_end_index as u64 + 1;
     }
