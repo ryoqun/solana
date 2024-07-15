822728b61cbc9d08a53cfe03de80161a928f4e0d
Ryo Onodera <ryoqun@gmail.com>
2024-07-15 12:23:31 +0900

    Revert "save"
    This reverts commit 771972f5a33e9307c3e81c85764eedf4730d750a.

--- perf/src/sigverify.rs
+++ perf/src/sigverify.rs
@@ -143,7 +143,7 @@ fn verify_packet(packet: &mut Packet, reject_non_vote: bool) -> bool {
         let Some(message) = packet.data(msg_start..) else {
             return false;
         };
-        if false && !signature.verify(pubkey, message) {
+        if !signature.verify(pubkey, message) {
             return false;
         }
         pubkey_start = pubkey_end;
