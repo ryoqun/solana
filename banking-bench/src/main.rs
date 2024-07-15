28dd383e6d45df16da416a8a9a571027afd22b40
Ryo Onodera <ryoqun@gmail.com>
2024-07-15 13:33:56 +0900

    save

--- banking-bench/src/main.rs
+++ banking-bench/src/main.rs
@@ -205,7 +205,7 @@ fn make_transfer_transaction_with_compute_unit_price(
         ComputeBudgetInstruction::set_compute_unit_limit(TRANSFER_TRANSACTION_COST),
     ];
     use solana_sdk::instruction::AccountMeta;
-    for _ in 0..25 {
+    for _ in 0..20 {
         instructions[0].accounts.push(AccountMeta::new(pubkey::new_rand(), false));
     }
     let message = Message::new(&instructions, Some(&from_pubkey));
