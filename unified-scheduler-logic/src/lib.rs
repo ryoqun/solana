da9f6f9e6e35769046658fe757fa11c9434200cc
Ryo Onodera <ryoqun@gmail.com>
2024-07-22 21:48:04 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -416,7 +416,7 @@ const_assert_eq!(mem::size_of::<UsageQueueToken>(), 0);
 type BlockedUsageCountToken = Token<ShortCounter>;
 const_assert_eq!(mem::size_of::<BlockedUsageCountToken>(), 0);
 
-pub type Index = u128;
+pub type Index = u64;
 
 /// Internal scheduling data about a particular task.
 #[derive(Debug)]
