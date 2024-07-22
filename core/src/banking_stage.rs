3e26ab8fe1bbf168852f14de6a4375d055fd8ed7
Ryo Onodera <ryoqun@gmail.com>
2024-07-22 21:52:52 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -744,7 +744,7 @@ impl BankingStage {
                                                 );
                                                 // wire cost tracker....
                                                 let i = /*((u64::MAX - priority) as u128) << 64
-                                                    | */task_ids[*i] /*as u128*/;
+                                                    | */task_ids[*i] as Index;
 
                                                 Some((tx, i))
                                             })
