0f9bdae20b77301d55b2a5c72d96d590b188c9d0
Ryo Onodera <ryoqun@gmail.com>
2024-07-23 13:17:11 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -416,7 +416,7 @@ impl Task {
 
 impl std::ops::Deref for Task {
     type Target = Arc<TaskInner>;
-    fn deref(&self) -> &<Self as Deref>::Target { todo!() }
+    fn deref(&self) -> &<Self as std::ops::Deref>::Target { todo!() }
 }
 const_assert_eq!(mem::size_of::<Task>(), 8);
 
