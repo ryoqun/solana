5080796e3e199109cce7bf116792b319e860a80a
Ryo Onodera <ryoqun@gmail.com>
2024-07-31 09:08:43 +0900

    save

--- perf/src/sigverify.rs
+++ perf/src/sigverify.rs
@@ -23,12 +23,14 @@ use {
     std::{convert::TryFrom, mem::size_of},
 };
 
+/*
 // Representing key tKeYE4wtowRb8yRroZShTipE18YVnqwXjsSAoNsFU6g
 const TRACER_KEY_BYTES: [u8; 32] = [
     13, 37, 180, 170, 252, 137, 36, 194, 183, 143, 161, 193, 201, 207, 211, 23, 189, 93, 33, 110,
     155, 90, 30, 39, 116, 115, 238, 38, 126, 21, 232, 133,
 ];
 const TRACER_KEY: Pubkey = Pubkey::new_from_array(TRACER_KEY_BYTES);
+*/
 const TRACER_KEY_OFFSET_IN_TRANSACTION: usize = 69;
 // Empirically derived to constrain max verify latency to ~8ms at lower packet counts
 pub const VERIFY_PACKET_CHUNK_SIZE: usize = 128;
@@ -318,7 +320,7 @@ pub fn check_for_tracer_packet(packet: &mut Packet) -> bool {
     };
     // Check for tracer pubkey
     match packet.data(first_pubkey_start..first_pubkey_end) {
-        Some(pubkey) if pubkey == TRACER_KEY.as_ref() => {
+        Some(pubkey) if pubkey == solana_sdk::packet::id().as_ref() => {
             packet.meta_mut().set_tracer(true);
             true
         }
