0e9b2a1f2474cbd415544c7d7628dd3bce567809
Ryo Onodera <ryoqun@gmail.com>
2024-08-05 22:51:26 +0900

    save

--- ledger-tool/src/args.rs
+++ ledger-tool/src/args.rs
@@ -28,6 +28,7 @@ use {
 use std::process::exit;
 use std::ffi::OsString;
 use solana_core::banking_trace::BankingSimulator;
+use log::warn;
 
 /// Returns the arguments that configure AccountsDb
 pub fn accounts_db_args<'a, 'b>() -> Box<[Arg<'a, 'b>]> {
