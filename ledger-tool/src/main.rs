823b4c052a37025838722a2c86e437420c1d6aed
Ryo Onodera <ryoqun@gmail.com>
2024-08-05 13:57:05 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -2383,7 +2383,7 @@ fn main() {
                         if let Ok(entries) = std::fs::read_dir(event_dir_path) {
                             let e2 = entries.flat_map(|r| r.ok().map(|r| r.file_name())).collect::<HashSet<OsString>>();
                             for events_file_name in (0..).map(|index| BankingSimulator::events_file_name(index)) {
-                                if !e2.remove(&events_file_name) {
+                                if !e2.remove(&events_file_name.into()) {
                                     break
                                 }
                             }
