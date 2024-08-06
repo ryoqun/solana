79f4800bc1cb5728fd863357c45ea12dac061c4a
Ryo Onodera <ryoqun@gmail.com>
2024-08-06 21:43:10 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -569,7 +569,7 @@ impl BankingSimulator {
             .working_bank_with_scheduler()
             .clone_with_scheduler();
 
-        let Ok((packet_batches_by_time, timed_hashes_by_slot)) = self.read_event_files();
+        let (packet_batches_by_time, timed_hashes_by_slot) = self.read_event_files().unwrap();
         let timed_hashes_by_slot = Arc::new(timed_hashes_by_slot);
         let bank_slot = bank.slot();
 
