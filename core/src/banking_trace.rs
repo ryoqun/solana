29af1facf0a96aa45f9be503e40d496b14d15161
Ryo Onodera <ryoqun@gmail.com>
2024-08-06 15:26:23 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -701,9 +701,9 @@ impl BankingSimulator {
                 let base_simulation_time = std::time::SystemTime::now();
                 for (&event_time, (label, batches_with_stats)) in timed_batches_to_send {
                     if event_time > base_event_time {
-                        let duration_since_base = event_time.duration_since(base_event_time).unwrap();
+                        let expected_duration_since_base = event_time.duration_since(base_event_time).unwrap();
                         // cache last base_simulation_time!
-                        while base_simulation_time.elapsed().unwrap() < duration_since_base {}
+                        while base_simulation_time.elapsed().unwrap() < expected_duration_since_base {}
                     }
 
                     let sender = match label {
