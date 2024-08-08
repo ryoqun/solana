0816d66765ab537b96b9569afca29c587eac4629
Ryo Onodera <ryoqun@gmail.com>
2024-08-08 14:02:28 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -833,7 +833,7 @@ impl BankingSimulator {
         for _ in 0..500 {
             if poh_recorder.read().unwrap().bank().is_none() {
                 let next_leader_slot = leader_schedule_cache.next_leader_slot(
-                    leader_pubkey,
+                    simulated_leader,
                     bank.slot(),
                     &bank,
                     Some(&self.blockstore),
