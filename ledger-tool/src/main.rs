c62eba937a0cabb5c511cbb24023e18bd5393fb3
Ryo Onodera <ryoqun@gmail.com>
2024-08-22 23:21:53 +0900

    save

--- ledger-tool/src/main.rs
+++ ledger-tool/src/main.rs
@@ -540,7 +540,7 @@ fn assert_capitalization(bank: &Bank) {
     assert!(bank.calculate_and_verify_capitalization(debug_verify));
 }
 
-fn load_banking_trace_events(ledger_path: &Path, arg_matches: &ArgMatches<'_>) -> BankingTraceEvents {
+fn load_banking_trace_events_or_exit(ledger_path: &Path, arg_matches: &ArgMatches<'_>) -> BankingTraceEvents {
     let file_pathes = parse_banking_trace_event_file_paths(
         arg_matches,
         banking_trace_path(&ledger_path),
@@ -1565,7 +1565,7 @@ fn main() {
 
                     let mut process_options = parse_process_options(&ledger_path, arg_matches);
                     if arg_matches.is_present("enable_hash_overrides") {
-                        let banking_trace_events = load_banking_trace_events(&ledger_path, arg_matches);
+                        let banking_trace_events = load_banking_trace_events_or_exit(&ledger_path, arg_matches);
                         process_options.hash_overrides = Some(banking_trace_events.hash_overrides().clone());
                     }
 
@@ -2398,20 +2398,7 @@ fn main() {
 
                     let mut process_options = parse_process_options(&ledger_path, arg_matches);
 
-                    let file_pathes = parse_banking_trace_event_file_paths(
-                        arg_matches,
-                        banking_trace_path(&ledger_path),
-                    );
-
-                    info!("Using: banking trace event files: {file_pathes:?}");
-                    let banking_trace_events = match BankingTraceEvents::load(file_pathes) {
-                        Ok(banking_trace_events) => banking_trace_events,
-                        Err(error) => {
-                            eprintln!("{error:?}");
-                            exit(1);
-                        }
-                    };
-
+                    let banking_trace_events = load_banking_trace_events_or_exit(&ledger_path, arg_matches);
                     process_options.hash_overrides = Some(banking_trace_events.hash_overrides().clone());
                     let slot = value_t!(arg_matches, "first_simulated_slot", Slot).unwrap();
 
