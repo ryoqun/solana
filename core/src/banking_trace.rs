acb328f99c9e1816cd65834b0b5ef35d219ef501
Ryo Onodera <ryoqun@gmail.com>
2024-08-22 15:56:08 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -625,6 +625,8 @@ impl BankingSimulator {
 
         let leader_schedule_cache = Arc::new(LeaderScheduleCache::new_from_bank(&bank));
         let parent_slot = bank.slot();
+        assert_eq!(parent_slot, self.parent_slot());
+
         let simulated_leader = leader_schedule_cache
             .slot_leader_at(self.first_simulated_slot, None)
             .unwrap();
