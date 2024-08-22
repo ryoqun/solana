2b49c4071740ab12bd6f61e11b9d2817967c70ef
Ryo Onodera <ryoqun@gmail.com>
2024-08-22 22:47:01 +0900

    save

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3179,9 +3179,9 @@ impl Bank {
                 self.fee_rate_governor.lamports_per_signature,
             );
         } else {
-            let g = self.hash_overrides.lock().unwrap().get_blockhash_override(self.slot()).copied().as_ref();
+            let g = self.hash_overrides.lock().unwrap().get_blockhash_override(self.slot()).copied();
             w_blockhash_queue.register_hash(
-                g.unwrap_or(blockhash),
+                g.as_ref().unwrap_or(blockhash),
                 self.fee_rate_governor.lamports_per_signature,
             );
         }
