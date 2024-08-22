b36bc57e9291a696dbfd21d76a1e66b10d8d1509
Ryo Onodera <ryoqun@gmail.com>
2024-08-22 16:18:16 +0900

    save

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -492,7 +492,7 @@ impl ClusterInfo {
         contact_info: ContactInfo,
         socket_addr_space: SocketAddrSpace,
     ) -> Self {
-        let kepair = Arc::new(Keypair::new()),
+        let kepair = Arc::new(Keypair::new());
         Self::do_new(contact_info, keypair, socket_addr_space)
     }
 
