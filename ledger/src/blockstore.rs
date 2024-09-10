0cb5b30c7cc1070d54dc283ab415f2a5be9bde49
Ryo Onodera <ryoqun@gmail.com>
2024-09-10 14:04:17 +0900

    save

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -3505,8 +3505,12 @@ impl Blockstore {
         &'a self,
         slot: &'a Slot,
         start_index: u32,
-        slot_meta: &'a SlotMeta,
     ) -> impl Iterator<Item = (Vec<Entry>, u32)> + 'a {
+        let slot_meta = self.meta_cf.get(slot)?;
+        if slot_meta.is_none() {
+            return [];
+        }
+
         assert!(!slot_meta.completed_data_indexes.contains(&(slot_meta.consumed as u32)));
         slot_meta.completed_data_indexes
             .range(start_index..slot_meta.consumed as u32)
