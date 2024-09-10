1c457d3298b39f69cf50559b6dae8b1ae7d79379
Ryo Onodera <ryoqun@gmail.com>
2024-09-10 15:25:56 +0900

    save

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -3569,7 +3569,7 @@ impl Blockstore {
                 })?;
             load_elapsed.stop();
             callback(load_elapsed, (entries, (end - start) as u64, last_shred.last_in_slot()))?;
-            load_elapsed = Measure::start("load_elapsed");
+            *load_elapsed = Measure::start("load_elapsed");
         }
         Ok(())
     }
