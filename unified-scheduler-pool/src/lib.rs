9731867b146b7df6c1e7a30838acb40c42864a50
Ryo Onodera <ryoqun@gmail.com>
2024-09-12 15:46:11 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -720,7 +720,7 @@ mod chained_channel {
     pub(super) fn unbounded<P, C: Clone>(
         initial_context: C,
     ) -> (ChainedChannelSender<P, C>, ChainedChannelReceiver<P, C>) {
-        const { assert!(false); mem::size_of::<ChainedChannel<P, C>>() };
+        const A: usize = const { assert!(false); mem::size_of::<ChainedChannel<P, C>>() };
         let (sender, receiver) = crossbeam_channel::unbounded();
         let (aux_sender, aux_receiver) = crossbeam_channel::unbounded();
         (
