c05e461ebfc694bdf8693af842a988380aae39e3
Ryo Onodera <ryoqun@gmail.com>
2024-09-13 23:38:23 +0900

    save

--- sdk/src/scheduling.rs
+++ sdk/src/scheduling.rs
@@ -1,7 +1,7 @@
 //! Primitive types relevant to transaction scheduling
 #![cfg(feature = "full")]
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum SchedulingMode {
     BlockVerification,
     BlockProduction,
