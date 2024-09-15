f61bf22411b3794e804590c90725cf2900d84560
Ryo Onodera <ryoqun@gmail.com>
2024-09-15 23:01:04 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -803,7 +803,7 @@ impl BankingStage {
                                             }
                                             */
                                             for (a, b) in ppp {
-                                                s.schedule_execution(a, b);
+                                                s.schedule_execution(&(a, b));
                                             }
                                         }
 
