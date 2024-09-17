66145bba060bda7efb2d81f7411fa8defac82b18
Ryo Onodera <ryoqun@gmail.com>
2024-09-17 23:10:43 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -570,6 +570,7 @@ impl Default for UsageQueueInner {
             // Note that large cap should be accompanied with proper scheduler cleaning after use,
             // which should be handled by higher layers (i.e. scheduler pool).
             blocked_usages_from_tasks: BTreeMap::new(),
+            blocked_usages_from_tasks2: BinaryHeap::with_capacity(128),
         }
     }
 }
