dddec96d9ce3446cc46ede3cf1ed4137fb2dd1d8
Ryo Onodera <ryoqun@gmail.com>
2024-09-18 13:33:12 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -102,7 +102,7 @@ use {
     solana_sdk::{pubkey::Pubkey, scheduling::SchedulingMode, transaction::SanitizedTransaction},
     static_assertions::const_assert_eq,
     std::{
-        collections::{BTreeMap, VecDeque},
+        collections::BTreeMap,
         mem,
         sync::Arc,
     },
