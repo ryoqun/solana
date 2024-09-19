e6880787fbc83f910a6c7972f08f4e1c94fa8fb4
Ryo Onodera <ryoqun@gmail.com>
2024-09-20 01:20:28 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -936,6 +936,7 @@ impl SchedulingStateMachine {
     }
 
     pub fn rebuffer_executing_task(&mut self, task: Task) {
+        assert!(task.has_blocked_usage(token));
         self.executing_task_count.decrement_self();
         self.buffered_task_total.increment_self();
         // assert task is executed?
