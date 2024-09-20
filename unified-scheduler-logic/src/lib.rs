34e029bf88b8d3be432e24ab7c32a2fb66d458da
Ryo Onodera <ryoqun@gmail.com>
2024-09-21 00:16:07 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -2286,7 +2286,7 @@ mod tests {
             .0
             .with_borrow_mut(&mut state_machine.usage_queue_token, |usage_queue| {
                 let task_index = task.index;
-                usage_queue.current_usage = Some(Usage::new(RequestedUsage::Writable, task));
+                usage_queue.current_usage = Some(Usage::Writable(task));
                 let _ = usage_queue.unlock(RequestedUsage::Readonly, task_index, &mut state_machine.count_token);
             });
     }
