42f101a80892f88441b21514b305c88de2810140
Ryo Onodera <ryoqun@gmail.com>
2024-09-20 23:35:56 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -982,7 +982,6 @@ impl SchedulingStateMachine {
     pub fn deschedule_task(&mut self, task: &Task) {
         assert_eq!((task.is_executed(&mut self.count_token), task.has_blocked_usage(&mut self.count_token)), (true, false));
         task.mark_as_unlocked(&mut self.count_token);
-        // assert blocked_usage_count == 0?
         self.executing_task_count.decrement_self();
         self.alive_task_count.decrement_self();
         self.executed_task_total.increment_self();
