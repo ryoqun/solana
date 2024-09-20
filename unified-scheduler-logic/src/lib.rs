533abaeb2e6d661bc0a37d6c9db4ba7a495b742a
Ryo Onodera <ryoqun@gmail.com>
2024-09-20 21:08:23 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -997,8 +997,8 @@ impl SchedulingStateMachine {
 
     fn try_reblock_task(blocking_task: &Task, blocked_task_count: &mut ShortCounter, token: &mut BlockedUsageCountToken) -> bool {
         if blocking_task.has_blocked_usage(token) {
-            assert!(!blocking_task.is_buffered(token));
-            true
+            assert!(blocking_task.is_buffered(token));
+            false
         //} else if blocking_task.is_buffered(token) {
         //    blocked_task_count.increment_self();
         //    true
