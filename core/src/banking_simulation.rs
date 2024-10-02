07f03f686e6851a285fea5c1fed705dbf9cfa59c
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 23:12:18 +0900

    save

--- core/src/banking_simulation.rs
+++ core/src/banking_simulation.rs
@@ -767,7 +767,11 @@ impl BankingSimulator {
         } else {
            retracer.create_unified_channel_tpu_vote(&non_vote_sender, &non_vote_receiver)
         };
-        let (gossip_vote_sender, gossip_vote_receiver) = retracer.create_channel_gossip_vote();
+        let (gossip_vote_sender, gossip_vote_receiver) = if !matches(block_production_method, BlockProductionMethod::UnifiedScheduler) {
+            retracer.create_channel_gossip_vote()
+        } else {
+            retracer.create_unified_channel_tpu_vote(&non_vote_sender, &non_vote_receiver)
+        };
 
         let connection_cache = Arc::new(ConnectionCache::new("connection_cache_sim"));
         let (replay_vote_sender, _replay_vote_receiver) = unbounded();
