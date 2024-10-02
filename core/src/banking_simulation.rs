7be5faa9378a331c284d363e3e5868977e700eda
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 22:42:57 +0900

    save

--- core/src/banking_simulation.rs
+++ core/src/banking_simulation.rs
@@ -887,7 +887,7 @@ impl BankingSimulator {
             bank_forks.clone(),
             prioritization_fee_cache,
             false,
-            panic!(),
+            solana_unified_scheduler_pool::MY_SCHEDULER_POOL.lock().unwrap().take().unwrap();
         );
 
         let sender_loop = SenderLoop {
