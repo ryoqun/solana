8d580c3ef63f7d337e4e1767e4593efa52803c54
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 21:41:26 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -703,7 +703,7 @@ impl BankingStage {
         }
         let id_generator = MonotonicIdGenerator::new();
         info!("create_block_producing_scheduler: start!");
-        let s: Arc<dyn solana_runtime::installed_scheduler_pool::BlockProducingScheduler>;
+        let s: Arc<BlockProducingUnifiedScheduler>;
         s = unified_scheduler_pool.create_banking_scheduler(&bank_forks.read().unwrap(), non_vote_receiver.clone(),
             |aaa| {
                 let bank = bank_forks.read().unwrap().working_bank();
