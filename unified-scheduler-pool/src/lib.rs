6fa95eb64c6eaafcc43fd099484bb1e8a2ee4507
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 21:08:11 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1388,7 +1388,7 @@ impl<S: SpawnableScheduler<TH>, TH: TaskHandler> ThreadManager<S, TH> {
                         let dummy_buffered_task_receiver =
                             dummy_receiver(state_machine.has_runnable_task() && !session_pausing);
 
-                        let (banking_packet_receiver, on_recv) = banking_context.as_ref().unwrap_or((&disconnected, || {}));
+                        let (banking_packet_receiver, on_recv) = banking_context.as_ref().unwrap_or((disconnected(), || {}));
 
                         // There's something special called dummy_unblocked_task_receiver here.
                         // This odd pattern was needed to react to newly unblocked tasks from
