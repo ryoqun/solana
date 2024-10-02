cf885ff86e0d6f8f0f4f71ec8aa558b0d96e6ab1
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 21:28:30 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -705,6 +705,7 @@ impl BankingStage {
         info!("create_block_producing_scheduler: start!");
         let s = unified_scheduler_pool.create_banking_scheduler(&bank_forks.read().unwrap(), non_vote_receiver.clone(),
             |aaa| {
+                let bank = bank_forks.read().unwrap().working_bank();
                 for pp in &aaa.0 {
                     // over-provision
                     let task_id =
