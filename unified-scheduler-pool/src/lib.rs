f28487f4e99f99f2fe751e2b8f932f821a14ad33
Ryo Onodera <ryoqun@gmail.com>
2024-10-02 21:52:15 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1509,23 +1509,20 @@ impl<S: SpawnableScheduler<TH>, TH: TaskHandler> ThreadManager<S, TH> {
                                 };
                                 let tasks = on_recv.as_mut().unwrap()((banking_packet));
                                 for task in tasks {
-                                if session_ending {
-                                    continue;
-                                }
-                                sleepless_testing::at(CheckPoint::NewTask(task.task_index()));
-                                if let Some(task) = state_machine.schedule_task(task) {
-                                    if !session_pausing {
-                                        //runnable_task_sender.send_aux_payload(task).unwrap();
-                                        runnable_task_sender.send_payload(task).unwrap();
-                                        "sc_i_task"
-                                    } else {
-                                        state_machine.rebuffer_executing_task(task);
-                                        "rebuffer"
+                                    if session_ending {
+                                        continue;
                                     }
-                                } else {
-                                    "new_b_task"
+                                    sleepless_testing::at(CheckPoint::NewTask(task.task_index()));
+                                    if let Some(task) = state_machine.schedule_task(task) {
+                                        if !session_pausing {
+                                            //runnable_task_sender.send_aux_payload(task).unwrap();
+                                            runnable_task_sender.send_payload(task).unwrap();
+                                        } else {
+                                            state_machine.rebuffer_executing_task(task);
+                                        }
+                                    }
                                 }
-                                }
+                                "banking"
                             }
                         };
                         let force_log = if !is_running && !state_machine.has_no_alive_task() {
