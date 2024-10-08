23d5efce53bc6dba67fc0483cf5b3301e74fc764
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 00:16:57 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1060,7 +1060,7 @@ impl SchedulingStateMachine {
                     let lockable: bool = task.with_pending_mut(&mut self.count_token, |c| {
                         c.pending_usage_queue.iter().all(|usage_queue| usage_queue.is_force_lockable(&mut self.usage_queue_token))
                     });
-                    dbg!(lockable);
+                    dbg!((task.index(), lockable));
                     //panic!("aaa");
                 }
                 false
