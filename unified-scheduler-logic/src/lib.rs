3ce448a1e53e8aecf8f52a05dcb7396cb3ed1e2e
Ryo Onodera <ryoqun@gmail.com>
2024-10-08 22:30:50 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -485,11 +485,11 @@ struct CounterWithStatus {
     status: TaskStatus,
     //#[bits(30)]
     count: u32,
-    pending_usage_queue: HashSet<*const UsageQueueInner>,
+    pending_usage_queue: HashSet<ByAddress<UsageQueue>>,
 }
 
 impl CounterWithStatus {
-    fn new(pending_usage_queue: HashSet<*const UsageQueueInner>) -> Self {
+    fn new(pending_usage_queue: HashSet<ByAddress<UsageQueue>>) -> Self {
         Self {
             status: TaskStatus::default(),
             count: u32::default(),
