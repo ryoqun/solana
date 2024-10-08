349ae5ebbcf7d44caf429e0556fcf92a2e3d18aa
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 00:14:01 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1060,6 +1060,7 @@ impl SchedulingStateMachine {
                     dbg!(lockable);
                     //panic!("aaa");
                 }
+                false
             },
         }
     }
