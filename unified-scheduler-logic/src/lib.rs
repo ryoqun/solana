2a8a2d3662c4e539b0ae33629afd4dd7d3fa1a6c
Ryo Onodera <ryoqun@gmail.com>
2024-10-08 15:54:46 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1221,7 +1221,7 @@ impl SchedulingStateMachine {
 
     fn unlock_usage_queues(&mut self, task: &Task) {
         for context in task.lock_contexts() {
-            context.map_ref(|context| {
+            context.borrow().map_ref(|context| {
             context.with_usage_queue_mut(&mut self.usage_queue_token, |usage_queue| {
                 let mut buffered_task_from_queue =
                     usage_queue.unlock(context, task.index(), &mut self.count_token);
