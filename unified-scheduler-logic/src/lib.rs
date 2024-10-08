e942c40770e431ec8585bec8c3261491c0574706
Ryo Onodera <ryoqun@gmail.com>
2024-10-08 22:28:16 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1278,7 +1278,7 @@ impl SchedulingStateMachine {
                     ) {
                         LockResult::Ok(()) => {
                             buffered_task_from_queue2.task().with_pending_mut(&mut self.count_token, |c| {
-                                c.pending_usage_queue.remove((&*usage_queue).ptr()).then_some(()).or_else(|| panic!());
+                                c.pending_usage_queue.remove(usage_queue as *const T).then_some(()).or_else(|| panic!());
                             });
                             // Try to further schedule blocked task for parallelism in the case of
                             // readonly usages
