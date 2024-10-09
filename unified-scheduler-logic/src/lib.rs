9deba8386257410e5852d2d7387c5c280e83a810
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 23:40:38 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1155,7 +1155,7 @@ impl SchedulingStateMachine {
                             continue;
                         }
                     };
-                    dbg!(("hey", scan_count, task.index(), prev_scan_task.index()));
+                    dbg!(("hey", scan_count, self.alive_tasks.len(), task.index(), prev_scan_task.index()));
                     scan_count -= 1;
                     if scan_count == 0 || *task == prev_scan_task {
                         break;
