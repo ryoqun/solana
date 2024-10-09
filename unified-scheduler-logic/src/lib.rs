c562cdd9d763279c41aa98bfe399f06873d1e824
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 11:53:19 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -990,7 +990,7 @@ impl UsageQueueInner {
             assert!(self.executing_count.is_zero());
             self.current_usage = None;
             while let Some(peeked_task) = self.blocked_usages_from_tasks.peek_mut() {
-                if !peeked_task.into().is_buffered(token) {
+                if !peeked_task.is_buffered(token) {
                     PeekMut::pop(peeked_task);
                 }
                 return peeked_task;
