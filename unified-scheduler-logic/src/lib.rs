c750e0ec5379e1fe6947b159e97205235a9cfebc
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 14:00:54 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1286,6 +1286,13 @@ impl SchedulingStateMachine {
         self.alive_tasks.remove(task).then_some(()).or_else(|| panic!());
         self.executed_task_total.increment_self();
         self.unlock_usage_queues(task);
+        if self.blocked_task_count() > 0 {
+            assert_gt!(
+                self.alive_task_count(),
+                self.blocked_task_count(),
+                "no deadlock"
+            );
+        }
     }
 
     fn try_reblock_task(blocking_task: &Task, blocked_task_count: &mut ShortCounter, token: &mut BlockedUsageCountToken) -> bool {
