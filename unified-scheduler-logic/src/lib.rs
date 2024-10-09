1d6d0619a775ec85f7d0efc761afc1f34a000979
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 13:41:51 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -938,7 +938,7 @@ impl UsageQueueInner {
                         c.pending_lock_contexts.insert(ByAddress(LockContext::new(u.clone(), RequestedUsage::Writable))).then_some(()).or_else(|| panic!());
                     });
                     self.insert_blocked_usage_from_task(
-                        UsageFromTask::Writable(current_task),
+                        UsageFromTask::Writable(current_task.clone()),
                     );
                     self.current_usage = Some(Usage::Readonly(ShortCounter::one()));
                 },
