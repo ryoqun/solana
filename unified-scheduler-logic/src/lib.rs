8845fe4c7902bcdcbf5395932a08d02f52d93fa5
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 13:34:22 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -928,7 +928,7 @@ impl UsageQueueInner {
                         );
                         //self.reblocked_lock_total.increment_self();
                     }
-                    self.current_usage = Usage::Writable(new_task);
+                    self.current_usage = Some(Usage::Writable(new_task));
                 },
             },
             Some(Usage::Writable(current_task)) => {
