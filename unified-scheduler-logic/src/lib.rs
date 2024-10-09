ce71baae6c2f63dfc79d30d27e7a69134c9ea74a
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 13:41:05 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -931,16 +931,29 @@ impl UsageQueueInner {
                     self.current_usage = Some(Usage::Writable(new_task));
                 },
             },
-            Some(Usage::Writable(current_task)) => {
-                let reblocked_task = std::mem::replace(current_task, new_task);
-                reblocked_task.increment_blocked_usage_count(count_token);
-                reblocked_task.with_pending_mut(count_token, |c| {
-                    c.pending_lock_contexts.insert(ByAddress(LockContext::new(u.clone(), RequestedUsage::Writable))).then_some(()).or_else(|| panic!());
-                });
-                self.insert_blocked_usage_from_task(
-                    UsageFromTask::Writable(reblocked_task),
-                );
-                //self.reblocked_lock_total.increment_self();
+            Some(Usage::Writable(current_task)) => match requested_usage {
+                RequestedUsage::Readonly => {
+                    current_task.increment_blocked_usage_count(count_token);
+                    current_task.with_pending_mut(count_token, |c| {
+                        c.pending_lock_contexts.insert(ByAddress(LockContext::new(u.clone(), RequestedUsage::Writable))).then_some(()).or_else(|| panic!());
+                    });
+                    self.insert_blocked_usage_from_task(
+                        UsageFromTask::Writable(reblocked_task),
+                    );
+                    self.current_usage = Some(Usage::Readonly(ShortCounter::one()));
+                },
+                },
+                RequestedUsage::Writable => {
+                    let reblocked_task = std::mem::replace(current_task, new_task);
+                    reblocked_task.increment_blocked_usage_count(count_token);
+                    reblocked_task.with_pending_mut(count_token, |c| {
+                        c.pending_lock_contexts.insert(ByAddress(LockContext::new(u.clone(), RequestedUsage::Writable))).then_some(()).or_else(|| panic!());
+                    });
+                    self.insert_blocked_usage_from_task(
+                        UsageFromTask::Writable(reblocked_task),
+                    );
+                    //self.reblocked_lock_total.increment_self();
+                }
             },
         }
     }
@@ -1107,7 +1120,11 @@ impl SchedulingStateMachine {
                         continue;
                     }
                     let force_lockable: bool = task.with_pending_mut(&mut self.count_token, |c| {
-                        c.pending_lock_contexts.iter().all(|pending_lock_context| pending_lock_context.is_force_lockable(&mut self.usage_queue_token))
+                        if c.pending_lock_contexts.is_empty() {
+                            false
+                        } else {
+                            c.pending_lock_contexts.iter().all(|pending_lock_context| pending_lock_context.is_force_lockable(&mut self.usage_queue_token))
+                        }
                     });
                     if force_lockable {
                         let p = task.with_pending_mut(&mut self.count_token, |c| {
