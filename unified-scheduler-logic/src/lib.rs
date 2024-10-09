f30e490be6266f2f79894edaf6f68ec109675037
Ryo Onodera <ryoqun@gmail.com>
2024-10-09 23:25:57 +0900

    save

--- unified-scheduler-logic/src/lib.rs
+++ unified-scheduler-logic/src/lib.rs
@@ -1142,7 +1142,7 @@ impl SchedulingStateMachine {
                 let mut prev_scan_task = self.last_scan_position.take().unwrap_or_else(|| self.alive_tasks.last().cloned().unwrap());
                 let mut task_iter = self.alive_tasks.range(..prev_scan_task.clone()).rev();
                 //let mut prev_task = None;
-                let task;
+                let mut task;
 
                 //for task in self.alive_tasks.range(..).rev() {
                 loop {
