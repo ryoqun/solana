51f8c728dcbe711b09fad6a3c50bb1068bb1dff6
Ryo Onodera <ryoqun@gmail.com>
2025-01-10 21:13:06 +0900

    no partition

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -14,30 +14,32 @@ shard=$1
 shift
 case "$shard" in
   1-of-4)
-    check_dcou1 --partition 1/9
-    check_dcou1 --partition 2/9
-    check_dcou1 --partition 3/9
-    check_dcou1 --partition 4/9
-    check_dcou1 --partition 5/9
-    check_dcou2 --partition 1/9
-    check_dcou2 --partition 2/9
-    check_dcou2 --partition 3/9
-    check_dcou2 --partition 4/9
-    check_dcou2 --partition 5/9
+    check_dcou1
+    #check_dcou1 --partition 1/9
+    #check_dcou1 --partition 2/9
+    #check_dcou1 --partition 3/9
+    #check_dcou1 --partition 4/9
+    #check_dcou1 --partition 5/9
+    #check_dcou2 --partition 1/9
+    #check_dcou2 --partition 2/9
+    #check_dcou2 --partition 3/9
+    #check_dcou2 --partition 4/9
+    #check_dcou2 --partition 5/9
     ;;
   2-of-4)
-    check_dcou1 --partition 6/9
-    check_dcou1 --partition 7/9
-    check_dcou2 --partition 6/9
-    check_dcou2 --partition 7/9
+    check_dcou2
+    #check_dcou1 --partition 6/9
+    #check_dcou1 --partition 7/9
+    #check_dcou2 --partition 6/9
+    #check_dcou2 --partition 7/9
     ;;
   3-of-4)
-    check_dcou1 --partition 8/9
-    check_dcou2 --partition 8/9
+    #check_dcou1 --partition 8/9
+    #check_dcou2 --partition 8/9
     ;;
   4-of-4)
-    check_dcou1 --partition 9/9
-    check_dcou2 --partition 9/9
+    #check_dcou1 --partition 9/9
+    #check_dcou2 --partition 9/9
     ;;
   *)
     echo "$0: unrecognized shard: $shard";
