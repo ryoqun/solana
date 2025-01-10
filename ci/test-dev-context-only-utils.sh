cc29067be29067be273fc134c5efe24ae6fa5880
Ryo Onodera <ryoqun@gmail.com>
2025-01-10 16:11:32 +0900

    rebalance

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -2,5 +2,33 @@
 
 set -eo pipefail
 
-scripts/check-dev-context-only-utils.sh check-all-targets "$@"
-scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
+check_dcou() {
+  scripts/check-dev-context-only-utils.sh check-all-targets "$@"
+  scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
+}
+
+shard=$1
+shift
+case "$shard" in
+  1-of-4)
+    check_dcou --partition 1/9
+    check_dcou --partition 2/9
+    check_dcou --partition 3/9
+    check_dcou --partition 4/9
+    check_dcou --partition 5/9
+    ;;
+  2-of-4)
+    check_dcou --partition 6/9
+    check_dcou --partition 7/9
+    ;;
+  2-of-4)
+    check_dcou --partition 8/9
+    ;;
+  4-of-4)
+    check_dcou --partition 9/9
+    ;;
+  *)
+    echo "$0: unrecognized shard: $shard";
+    exit 1
+    ;;
+esac
