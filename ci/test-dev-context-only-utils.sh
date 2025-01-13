b133168c9d683b03e0128288f3e0d142894cbeba
Ryo Onodera <ryoqun@gmail.com>
2025-01-13 13:20:24 +0900

    try local sccache

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -7,6 +7,8 @@ check_dcou() {
   scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
 }
 
+unset SCCACHE_GCS_KEY_PATH SCCACHE_GCS_BUCKET SCCACHE_GCS_RW_MODE SCCACHE_GCS_KEY_PREFIX
+
 shard=$1
 shift
 echo before:
@@ -41,3 +43,38 @@ echo end:
 ls -ltr --full-time ./target || true
 du -shc ./target/* || true
 sccache --show-stats
+sccache --stop-server
+
+echo before2:
+ls -ltr --full-time ./target || true
+du -shc ./target/* || true
+sccache --show-stats
+case "$shard" in
+  1-of-4)
+    check_dcou --partition 1/9
+    check_dcou --partition 2/9
+    check_dcou --partition 3/9
+    check_dcou --partition 4/9
+    check_dcou --partition 5/9
+    ;;
+  2-of-4)
+    check_dcou --partition 6/9
+    check_dcou --partition 7/9
+    ;;
+  3-of-4)
+    check_dcou --partition 8/9
+    ;;
+  4-of-4)
+    check_dcou --partition 9/9
+    ;;
+  *)
+    echo "$0: unrecognized shard: $shard";
+    exit 1
+    ;;
+esac
+
+echo end2:
+ls -ltr --full-time ./target || true
+du -shc ./target/* || true
+sccache --show-stats
+sccache --stop-server
