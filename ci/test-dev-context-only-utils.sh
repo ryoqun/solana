e0958b80f792aa35012842e6f9d0ce66a4f9fdda
Ryo Onodera <ryoqun@gmail.com>
2025-01-13 23:18:59 +0900

    oops

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -52,21 +52,16 @@ rm -rf ./target
 sccache --show-stats
 case "$shard" in
   1-of-4)
-    check_dcou --partition 1/9
-    check_dcou --partition 2/9
-    check_dcou --partition 3/9
-    check_dcou --partition 4/9
-    check_dcou --partition 5/9
+    check_dcou --partition 1/4
     ;;
   2-of-4)
-    check_dcou --partition 6/9
-    check_dcou --partition 7/9
+    check_dcou --partition 2/4
     ;;
   3-of-4)
-    check_dcou --partition 8/9
+    check_dcou --partition 3/4
     ;;
   4-of-4)
-    check_dcou --partition 9/9
+    check_dcou --partition 4/4
     ;;
   *)
     echo "$0: unrecognized shard: $shard";
